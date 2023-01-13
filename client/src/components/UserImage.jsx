37b50556eb306955346627046ddec978c8b0b8c0
King-tut <jasonpitts63@gmail.com>
2023-01-13 02:49:11 -0500

    user related UI and component

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material";
+
+const UserImage = ({image, size="60px"}) =>{
+
+    return (
+        <Box width={size} height={size}>
+
+            <img 
+            width={size}
+            height={size}
+            style={{objectFit: "cover", borderRadius: "50%"}}
+            alt="user"
+            src={`http://localhost:5000/assets/${image}`} 
+            
+            
+            />
+        </Box>
+    )
+
+
+}
+
+export default UserImage;
